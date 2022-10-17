4598b5e5cdea101f14e66766ce4328a959708028
orishac <62713593+orishac@users.noreply.github.com>
2022-10-17 19:43:41 +0300

    only 1 part left

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -16,7 +16,7 @@ const createTicket = async (ticketData, toekn) => {
 }
 
 //Get user tickets
-const getTickets = async (toekn) => {
+const getTickets = async (token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`
@@ -28,9 +28,41 @@ const getTickets = async (toekn) => {
     return response.data
 }
 
+//Get user ticket
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(API_URL + ticketId, config)
+
+    return response.data
+}
+
+// Close ticket
+const closeTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.put(
+        API_URL + ticketId, 
+        { status: 'close' }, 
+        config
+    )
+
+    return response.data
+}
+
 const ticketService = {
     createTicket,
     getTickets,
+    getTicket,
+    closeTicket,
 }
 
 export default ticketService
