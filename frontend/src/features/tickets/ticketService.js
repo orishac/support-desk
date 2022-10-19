cc3bafc02ae501f62b7b5d97d14348ab2cc3e996
orishac <62713593+orishac@users.noreply.github.com>
2022-10-19 14:00:26 +0300

    fixed mispelling

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = '/api/tickets/'
 
 //Create new ticket
-const createTicket = async (ticketData, toekn) => {
+const createTicket = async (ticketData, token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`
